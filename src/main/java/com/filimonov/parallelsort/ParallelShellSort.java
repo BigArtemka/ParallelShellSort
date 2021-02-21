3f36a3a9cbbd4a5dac7d2b44ef80bad7d752044c
BigArtemka <dak.112008@gmail.com>
2021-02-21 15:41:12 +0300

    Добавлен ExecutorService

--- src/main/java/com/filimonov/parallelsort/ParallelShellSort.java
+++ src/main/java/com/filimonov/parallelsort/ParallelShellSort.java
@@ -1,9 +1,10 @@
 package com.filimonov.parallelsort;
 
-import java.net.Inet4Address;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author Artem Filimonov
@@ -11,6 +12,10 @@ import java.util.List;
 public class ParallelShellSort {
 
     public static void sort(int[] array, int threadsCount) throws InterruptedException {
+        if (threadsCount < 2) {
+            sort(array);
+            return;
+        }
         int h = 1;
         while (h * 3 < array.length)
             h = h * 3 + 1;
@@ -18,24 +23,26 @@ public class ParallelShellSort {
     }
 
     private static void sort(int[] array, int threadsCount, int h) throws InterruptedException {
-        List<Thread> threads = new ArrayList<>();
 
-        if (h < 2) {
-            insertSort(array);
-            return;
-        }
+        ExecutorService executor = Executors.newFixedThreadPool(threadsCount);
+        List<Callable<Object>> todo = new ArrayList<>(threadsCount);
 
         for (int i = 0; i < threadsCount && i < h; i++) {
-            int finalI = i;
-            threads.add(new Thread(() -> insertSort(array, h, finalI, threadsCount)));
-            threads.get(i).start();
+            todo.add(new Task(array, h, i, threadsCount));
         }
 
-        for (Thread thread : threads) {
-            thread.join();
+        h /= 3;
+
+        while (h > 2) {
+            for (int i = 0; i < threadsCount && i < h; i++) {
+                todo.set(i, new Task(array, h, i, threadsCount));
+            }
+            executor.invokeAll(todo);
+            h /= 3;
         }
 
-        sort(array, threadsCount, h / 3);
+        insertSort(array);
+        executor.shutdown();
     }
 
     private static void insertSort(int[] array, int h, int i, int threadCount) {
@@ -43,14 +50,7 @@ public class ParallelShellSort {
         int length = array.length;
         for (int k = i; k < h; k += threadCount)
             for (i = k; i < length; i += h) {
-                for (int j = i; j >= h; j = j - h) {
-                    if (array[j] < array[j - h]) {
-                        temp = array[j];
-                        array[j] = array[j - h];
-                        array[j - h] = temp;
-                    } else
-                        break;
-                }
+                sortIter(array, h, i);
             }
     }
 
@@ -66,6 +66,55 @@ public class ParallelShellSort {
                 }
             }
             array[i + 1] = value;
+        }
+    }
+
+    static class Task implements Callable<Object> {
+
+        private final int[] array;
+        private final int finalH, finalI, threadsCount;
+
+        public Task(int[] array, int finalH, int finalI, int threadsCount) {
+            this.array = array;
+            this.finalH = finalH;
+            this.finalI = finalI;
+            this.threadsCount = threadsCount;
+        }
+
+        @Override
+        public Object call() {
+            insertSort(array, finalH, finalI, threadsCount);
+            return Thread.currentThread().getName();
+        }
+    }
+
+    private static void sort(int[] array) {
+        int h = 1;
+        while (h * 3 < array.length)
+            h = h * 3 + 1;
+
+        while (h >= 1) {
+            hSort(array, h);
+            h = h / 3;
+        }
+    }
+
+    private static void hSort(int[] array, int h) {
+        int length = array.length;
+        for (int i = h; i < length; i++) {
+            sortIter(array, h, i);
+        }
+    }
+
+    private static void sortIter(int[] array, int h, int i) {
+        int temp;
+        for (int j = i; j >= h; j = j - h) {
+            if (array[j] < array[j - h]) {
+                temp = array[j];
+                array[j] = array[j - h];
+                array[j - h] = temp;
+            } else
+                break;
         }
     }
 
